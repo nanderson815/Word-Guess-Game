3f8aebb9303a2c5573f683c54c72f65bbd1b694b
Noah A <nanderson815@gmail.com>
2018-11-30 17:04:52 -0500

    Updated 11 30 18

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 
+// Gets lettersGuessed element from HTML
 var lettersGuessed = document.getElementById("lettersGuessed");
 
 
@@ -6,25 +7,50 @@ var lettersGuessed = document.getElementById("lettersGuessed");
 var guessesCounter = document.getElementById("guessesCounter");
 var guessesRemaining = 10;
 
-var words = ["Uga", "Sanford", "Rodrigo", "Hedges", "Kirby", "Chubb", "Gurley",];
+// Array of hangman words
+var words = ["uga", "sanford", "rodrigo", "hedges", "kirby", "chubb", "gurley",];
+
+// grabs correct letters HTML Id
+var correctLetters = document.getElementById("correctLetters");
+
+// Holds Activeword as string
+var activeWord;
+
+// Array to hold unique user letter guesses
 var userGuesses = [];
 
+// Array to hold guessed letters and dashes for unknown letters.
+var wordInProgress = [];
 
+// Selects random word from words array on load
 function selectWord() {
-    var activeWord = words[Math.floor(Math.random() * words.length)];
-    console.log(activeWord);
+    activeWord = words[Math.floor(Math.random() * words.length)];
+    for (i = 0; i < activeWord.length; i++) {
+        var unknown = "—";
+        wordInProgress.push(unknown);
+        correctLetters.innerHTML = wordInProgress.join("  ");
+    }
 }
 selectWord();
+console.log(activeWord);
 
+
+// executes on key down....
 document.onkeydown = function (event) {
-
-
 
     if (userGuesses.length < 10) {
         var Guess = event.key;
         if (userGuesses.indexOf(Guess) === -1) {
             userGuesses.push(Guess);
-            lettersGuessed.innerHTML = userGuesses;
+
+            for (i = 0; i < wordInProgress.length; i++) {
+                if (activeWord[i] === Guess) {
+                    wordInProgress[i] = Guess;
+                }
+                console.log(wordInProgress);
+            }
+
+            lettersGuessed.innerHTML = userGuesses.join(", ");
             guessesRemaining -= 1;
             guessesCounter.innerHTML = guessesRemaining;
         }
