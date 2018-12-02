0b52186191323a8498486d7a7ca333e91e5871ce
Noah A <nanderson815@gmail.com>
2018-12-02 17:41:33 -0500

    updated 12 2 18

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ var guessesCounter = document.getElementById("guessesCounter");
 var guessesRemaining = 10;
 
 // Array of hangman words
-var words = ["uga", "sanford", "rodrigo", "hedges", "kirby", "chubb", "gurley", "athens", "dawgs", "georgia", "sec", "football", "bulldawgs"];
+var words = ["uga", "sanford", "rodrigo", "hedges", "kirby", "chubb", "gurley", "athens", "dawgs", "bulldawgs"];
 
 // grabs correct letters HTML Id
 var correctLetters = document.getElementById("correctLetters");
@@ -21,6 +21,8 @@ var userGuesses = [];
 
 // Array to hold guessed letters and dashes for unknown letters.
 var wordInProgress = [];
+
+var leftImage = document.getElementById("leftImage");
 
 // Links audio
 var audio = new Audio('assets/audio/fightsong.mp3');
@@ -42,6 +44,7 @@ console.log(activeWord);
 function resetGame() {
     // Stops the music.
     audio.pause();
+    audio.currentTime = 0;
     // Removes all guessed letters and replace with dashes.
     wordInProgress = [];
     userGuesses = [];
@@ -51,6 +54,7 @@ function resetGame() {
     guessesCounter.innerHTML = guessesRemaining;
     // Hides the play again button.
     resetButton.style.visibility = "hidden";
+    leftImage.src = "assets/images/dummyImage.png";
 }
 
 // On Click resets game
@@ -58,13 +62,11 @@ var resetButton = document.getElementById("resetButton");
 resetButton.onclick = function reset() {
     resetGame();
     selectWord();
+    console.log(activeWord);
 }
 
 // executes on key down....
 document.onkeydown = function (event) {
-
-
-
     // Only Allows the user 10 guesses.
     if (userGuesses.length < 10) {
         // logs user keystroke as Guess
@@ -93,14 +95,40 @@ document.onkeydown = function (event) {
 
 // Checks to see if guess is correct by looking for "-" in wordInProgress array.
 document.onkeyup = function (event) {
+    // If array still contains "-" when user is out of guesses...
     if (wordInProgress.indexOf('—') > -1) {
         if (userGuesses.length == 10) {
+            // Alert "you lose!", show reset button.
             alert("You lose! The correct word was: " + activeWord);
             resetButton.style.visibility = "visible";
         }
+        // If dashes are gone, than the user has correctly guessed the word.
     } else {
-        console.log("guess is correct.");
+        // play UGA fight song. Show reset button.
         audio.play();
         resetButton.style.visibility = "visible";
+        if (activeWord == "rodrigo") {
+            leftImage.src = "assets/images/rodrigo.jpg";
+        } else if  (activeWord == "uga"){
+            leftImage.src = "assets/images/uga.jpg";
+        } else if (activeWord == "sanford"){
+            leftImage.src = "assets/images/sanford.jpg";
+        } else if (activeWord == "hedges"){
+            leftImage.src = "assets/images/hedges.jpg";
+        } else if (activeWord == "kirby"){
+            leftImage.src = "assets/images/kirby.jpg";
+        } else if (activeWord == "chubb"){
+            leftImage.src = "assets/images/chubb.jpg";
+        } else if (activeWord == "gurley"){
+            leftImage.src = "assets/images/gurley.jpg";
+        } else if (activeWord == "athens"){
+            leftImage.src = "assets/images/athens.jpeg";
+        } else if (activeWord == "dawgs"){
+            leftImage.src = "assets/images/dawgs.jpg";
+        } else if (activeWord == "bulldawgs"){
+            leftImage.src = "assets/images/bulldawgs.jpg";
+        } else {
+            leftImage.src = "assets/images/football.jpg";
+        }
     }
 }
